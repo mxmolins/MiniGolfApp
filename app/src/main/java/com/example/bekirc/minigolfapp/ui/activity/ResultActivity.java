180dc663d9be93bac80fc957a4275c5f9a1afc5c
bekirc <bkrcinar@gmail.com>
2016-02-24 11:54:31 +0100

    share buttons improvement

--- app/src/main/java/com/example/bekirc/minigolfapp/ui/activity/ResultActivity.java
+++ app/src/main/java/com/example/bekirc/minigolfapp/ui/activity/ResultActivity.java
@@ -1,7 +1,10 @@
 package com.example.bekirc.minigolfapp.ui.activity;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
@@ -18,8 +21,10 @@ import com.example.bekirc.minigolfapp.ui.fragment.dialog.SuggestionsDialogFragme
 import com.example.bekirc.minigolfapp.ui.misc.DividerItemDecoration;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.InjectView;
+import butterknife.OnClick;
 
 /**
  * @author bekirc on 03.12.15.
@@ -104,6 +109,103 @@ public class ResultActivity extends BaseActionBarActivity implements GameFinishe
         Intent intent = new Intent(this, HomeActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
+    }
+
+    @OnClick(R.id.fab_share_on_facebook)
+    void onFacebookShareButtonclicked() {
+        //todo
+    }
+
+    @OnClick(R.id.fab_share_on_whatsapp)
+    void onWhatsappShareButtonClicked() {
+        //todo
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        List<ResolveInfo> resolveInfoList = getPackageManager().queryIntentActivities(shareIntent, 0);
+        if (!resolveInfoList.isEmpty()) {
+            for (ResolveInfo resInfo : resolveInfoList) {
+                String packageName = resInfo.activityInfo.packageName;
+                if (packageName.toLowerCase().contains("whatsapp")) {
+                    Intent intent = new Intent();
+                    intent.setComponent(new ComponentName(packageName, resInfo.activityInfo.name));
+                    intent.setAction(Intent.ACTION_SEND);
+                    intent.setPackage(packageName);
+
+                    Intent chooserIntent = Intent.createChooser(intent, getResources().getString(R.string.SHARE_ON_WHATSAPP));
+                    startActivity(chooserIntent);
+                    break;
+                }
+            }
+        }
+
+        //todo there is no whats app
+    }
+
+    @OnClick(R.id.fab_share_on_others)
+    void onShareOnOtherButtonClicked() {
+        //todo
+        sendShareIntent();
+    }
+
+    private void sendShareIntent() {
+        Intent shareIntent = createShareIntent();
+        startActivity(shareIntent);
+    }
+
+    private Intent createShareIntent() {
+        List<Intent> targetShareIntents = new ArrayList<>();
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        List<ResolveInfo> resolveInfoList = getPackageManager().queryIntentActivities(shareIntent, 0);
+        if (!resolveInfoList.isEmpty()) {
+            for (ResolveInfo resInfo : resolveInfoList) {
+                String packageName = resInfo.activityInfo.packageName;
+
+                Intent intent = new Intent();
+                intent.setComponent(new ComponentName(packageName, resInfo.activityInfo.name));
+                intent.setAction(Intent.ACTION_SEND);
+                intent.setPackage(packageName);
+
+//                String msg = getString(R.string.STRING_SHARING_PHRASE) + " " + completeUrl + " \n\n";
+//
+//                if (packageName.contains("twitter")) {
+//                    intent.setType("text/plain");
+//
+//                    intent.putExtra(Intent.EXTRA_TEXT, msg);
+//                    targetShareIntents.add(intent);
+//                } else {
+//
+//                    msg = msg + getString(R.string.STRING_SEARCH_COLOR) + ": " + vehicle.getColorFamilyName();
+//
+//                    msg = msg + "\n" + completeUrl;
+//
+//                    if (packageName.contains("android.gm") || packageName.contains("android.email")) {
+//                        intent.setType("message/rfc822");
+//
+//
+//                        intent.putExtra(Intent.EXTRA_TEXT, msg);
+//                        targetShareIntents.add(intent);
+//
+//                    } else {
+//
+//                        intent.setType("text/plain");
+//
+//                        intent.putExtra(Intent.EXTRA_TEXT, msg);
+//                        targetShareIntents.add(intent);
+//                    }
+//                }
+            }
+
+            if (!targetShareIntents.isEmpty()) {
+                Intent chooserIntent = Intent.createChooser(targetShareIntents.remove(0), getResources().getString(R.string.SHARE_ON_OTHER));
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, targetShareIntents.toArray(new Parcelable[targetShareIntents.size()]));
+                return chooserIntent;
+            }
+
+        }
+        return shareIntent;
     }
 
     @Override
